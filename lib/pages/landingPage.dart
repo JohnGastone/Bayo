5207fbf672f342f6bed708a62eb30400066a7f14
JohnGastone <johngastone11@gmail.com>
2024-06-16 20:56:28 +0300

    +++

--- lib/pages/landingPage.dart
+++ lib/pages/landingPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -13,15 +14,18 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Shop your kits here',
+                style: GoogleFonts.poppins(
+                    fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
         ),
       ),
     );

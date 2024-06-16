9764996277e71281b3adceec7f5e04bb4d69586d
JohnGastone <johngastone11@gmail.com>
2024-06-16 23:33:35 +0300

    +++

--- lib/pages/landingPage.dart
+++ lib/pages/landingPage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -16,8 +17,20 @@ class _LandingPageState extends State<LandingPage> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Row(
-          children: [],
+        title: Padding(
+          padding: const EdgeInsets.only(top: 10, left: 15, right: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.menu),
+              Image.asset(
+                './assets/nike.png',
+                height: 50,
+                width: 200,
+              ),
+              Icon(CupertinoIcons.cart)
+            ],
+          ),
         ),
       ),
       body: Center(
@@ -30,6 +43,38 @@ class _LandingPageState extends State<LandingPage> {
                 style: GoogleFonts.poppins(
                     fontSize: 20, fontWeight: FontWeight.w500),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Row(
+                  children: [
+                    Icon(CupertinoIcons.sort_down_circle),
+                    Image.asset(
+                      './assets/real-madrid.png',
+                      height: 50,
+                      width: 150,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      './assets/liverpool.png',
+                      height: 50,
+                      width: 150,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      './assets/barcelona.png',
+                      height: 50,
+                      width: 150,
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),

06305987bf5c75fe0693e509b56e94eff8c4deda
JohnGastone <johngastone11@gmail.com>
2024-06-17 22:51:55 +0300

    +++

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:bayo/pages/landingPage.dart';
 import 'package:flutter/material.dart';
 
@@ -12,10 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: const LandingPage(),
     );
   }
